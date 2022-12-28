d75c211739c69bc76d8190f2c1432f2ace82328a
Hungry_munk <abbaszadeharsam@gmail.com>
2022-12-29 07:46:10 +1100

    adding nessecery file

---
+++ classes.js
@@ -0,0 +1,13 @@
+export class Node {
+    constructor(value = null, left = null, right = null) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+export class Tree {
+    constructor () {
+        
+    }
+}
